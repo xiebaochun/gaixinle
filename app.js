108036d257634f79143a6fb13c5d2cbeea823fa5
xbc <you@example.com>
2018-07-27 12:34:41 +0800

    update

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ var path = require('path');
 var web = express();
 web.use(express.static(path.join(__dirname,'/')));
 
-http.createServer(web).listen('3003',function(){
-	console.log('server at port 3001');
+http.createServer(web).listen('3033',function(){
+	console.log('server at port 3033');
 });
